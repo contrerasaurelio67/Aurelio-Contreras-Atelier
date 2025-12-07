422ac6dda061ad62872ea2820e0067ee667da9b2
contrerasaurelio67 <contrerasaurelio67@gmail.com>
2025-12-07 10:48:49 -0400

    Actualizacion automatica

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,12 @@
 @echo off
+git pull origin main
 git add .
-git commit -m "Actualización automática"
+git commit -m "Actualizacion automatica"
 git push
 
 echo.
 echo ============================================
-echo   ✅ Deploy completado correctamente
-echo   Los cambios ya están en GitHub y Netlify
+echo   Deploy completado correctamente
+echo   Los cambios ya estan en GitHub y Netlify
 echo ============================================
 pause
